13af5ff5123a4de0839e3dcfc54b8c1cad98da86
Ben Gyori <ben.gyori@gmail.com>
2015-04-12 18:39:27 -0400

    Modifications for exponential test steps

--- figure_seqindetdelta.m
+++ figure_seqindetdelta.m
@@ -9,16 +9,18 @@ if reload
 
     rs = [linspace(0.01,0.70,8), ...
           linspace(0.75,1,20)];
-
+    rms = min([1./(1-rs);1./rs]);
     deltas = [0.05,0.08,0.1];
 
     errs = zeros(length(rs),length(deltas));
     ET = 1e8*ones(length(rs),length(deltas));
     T = nan(nRuns,length(rs),length(deltas));
     Nfix = zeros(nRuns,length(deltas));
+    
+    xi = 0.3;
 
     for i=1:nRuns
-        fname = sprintf('results/odemcmc-jakstat-1-2000000-50000-1-%d.mat',i);
+        fname = sprintf('~/data/phd/mcmchyp/odemcmc-jakstat-1-2000000-50000-1-%d.mat',i);
         fprintf('Reading %s\n',fname);
         load(fname);
         sfx = cumsum(f);
@@ -27,17 +29,21 @@ if reload
 
         for j=1:length(rs)
             r = rs(j);
+			rm = rms(j);
             sfxz = sfx-(1:N)'*r;
             for k=1:length(deltas)
                 delta = deltas(k);
-                M = log(epsilon*gamma*delta^2/2)/ (-2*gamma*delta - (gamma*delta^2)/(1-r));
-                tj = find(abs(sfxz)>M,1,'first');
+                M = log(2/sqrt(epsilon*xi))/(2*gamma*delta);
+				n0 = floor(M*rm);
+				imax = floor(log(N/n0)/log(1+xi));
+				ntest = floor(n0*(1+xi).^(1:imax));
+				tj = find(abs(sfxz(ntest))>M,1,'first');
                 Nfix(i,k) = log(1/epsilon)/(gamma*delta^2);
                 if ~isempty(tj)
-                    T(i,j,k) = tj;
-                    if (r < mtrue) && (sfxz(tj) < 0)
+                    T(i,j,k) = ntest(tj);
+                    if (r < mtrue) && (sfxz(ntest(tj)) < 0)
                         errs(j,k) = errs(j,k) + 1;
-                    elseif (r > mtrue) && (sfxz(tj) > 0)
+                    elseif (r > mtrue) && (sfxz(ntest(tj)) > 0)
                         errs(j,k) = errs(j,k) + 1;
                     end
                 end
@@ -54,7 +60,9 @@ if reload
             end
 
         end
-    end
+	end
+	save('results/figure_seqindetdelta_dat.mat',...
+		'ET','xi','rs','nRuns','Nfix','epsilon','deltas','mtrue','errs')
 else
     load('results/figure_seqindetdelta_dat.mat');
 end
@@ -77,6 +85,7 @@ plot(0:0.2:1,Nfixm(2)*ones(1,6),'o--','markers',markersize);
 plot(0:0.2:1,Nfixm(3)*ones(1,6),'^--','markers',markersize);
 legend({'\delta=0.05','\delta=0.08','\delta=0.1','seq.','fixed'});
 ylim([0 1e6]);
+set(gca,'yscale','linear')
 xlabel('Probability threshold (r)','FontSize',12);
 ylabel('Average stopping time (number of samples)','FontSize',12);
 set(findall(gcf,'type','text'),'FontSize',16)
--- figure_seqindeteps.m
+++ figure_seqindeteps.m
@@ -9,35 +9,43 @@ if reload
 
 	rs = [linspace(0.01,0.70,8), ...
 		  linspace(0.75,1,20)];
-
+	rms = min([1./(1-rs);1./rs]);
 	epsilons = [0.01,0.02,0.05];
 
 	errs = zeros(length(rs),length(epsilons));
 	ET = 1e8*ones(length(rs),length(epsilons));
 	T = nan(nRuns,length(rs),length(epsilons));
 	Nfix = zeros(nRuns,length(epsilons));
+	
+	xi = 0.3;
 
 	for i=1:nRuns
-		fname = sprintf('results/odemcmc-jakstat-1-2000000-50000-1-%d.mat',i);
+		fname = sprintf('~/data/phd/mcmchyp/odemcmc-jakstat-1-2000000-50000-1-%d.mat',i);
 		fprintf('Reading %s\n',fname);
 		load(fname);
+		n = length(f);
 		sfx = cumsum(f);
 		N = length(f);	
 		gamma = gammas(i);
 
 		for j=1:length(rs)
 			r = rs(j);
-			sfxz = sfx-(1:N)'*r;
+			rm = rms(j);
+			
+			sfxz = sfx-(1:n)'*r;
 			for k=1:length(epsilons)
 				epsilon = epsilons(k);
-				M = log(epsilon*gamma*delta^2/2)/ (-2*gamma*delta - (gamma*delta^2)/(1-r));
-				tj = find(abs(sfxz)>M,1,'first');
+				M = log(2/sqrt(epsilon*xi))/(2*gamma*delta);
+				n0 = floor(M*rm);
+				imax = floor(log(N/n0)/log(1+xi));
+				ntest = floor(n0*(1+xi).^(1:imax));
+				tj = find(abs(sfxz(ntest))>M,1,'first');
 				Nfix(i,k) = log(1/epsilon)/(gamma*delta^2);
 				if ~isempty(tj)
-					T(i,j,k) = tj;
-					if (r < mtrue) && (sfxz(tj) < 0)
+					T(i,j,k) = ntest(tj);
+					if (r < mtrue) && (sfxz(ntest(tj)) < 0)
 						errs(j,k) = errs(j,k) + 1;
-					elseif (r > mtrue) && (sfxz(tj) > 0)
+					elseif (r > mtrue) && (sfxz(ntest(tj)) > 0)
 						errs(j,k) = errs(j,k) + 1;
 					end
 				end
@@ -54,6 +62,8 @@ if reload
 			end
 		end
 	end
+	save('results/figure_seqindeteps_dat.mat',...
+		'ET','xi','rs','nRuns','Nfix','epsilons','delta','mtrue','errs')
 else
 	load('results/figure_seqindeteps_dat.mat');
 end
@@ -75,8 +85,9 @@ plot(0:0.2:1,Nfixm(2)*ones(1,6),'o--','markers',markersize);
 plot(0:0.2:1,Nfixm(3)*ones(1,6),'^--','markers',markersize);
 legend({'\epsilon=0.01','\epsilon=0.02','\epsilon=0.05','seq.','fixed'});
 ylim([0 1e6]);
+set(gca,'yscale','linear')
 xlabel('Probability threshold (r)','FontSize',12);
 ylabel('Average stopping time (number of samples)','FontSize',12);
 set(findall(gcf,'type','text'),'FontSize',16)
 set(gca,'FontSize',14)
-saveas(fh, 'figs/figure_sequindeteps','eps');
+saveas(fh, 'figs/figure_seqindeteps','eps');
